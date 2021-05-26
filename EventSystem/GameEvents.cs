f93df8c6db5514eb404796ba445f239d2b8d4c43
freddiejbawden <freddiejbawden@gmail.com>
2021-05-26 20:29:57 +0100

    Create GameEvents.cs

---
+++ EventSystem/GameEvents.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+
+public enum GameState
+{
+    FISHING,
+    BOAT
+}
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    /* Format 
+     * 
+     * public event Action<T> onEventTrigger;
+     * public void EventTrigger(T t)
+     * {
+     *      if (onEventTrigger != null)
+     *      {
+     *          onEventTrigger(t)
+     *      }
+     * }
+     */
+    public event Action<int> onLoadLevel;
+    public void LoadLevel(int levelIdx)
+    {
+        if (onLoadLevel != null)
+        {
+            onLoadLevel(levelIdx);
+        }
+    }
+
+    public event Action onLoadNextLevel;
+    public void LoadNextLevel()
+    {
+        if (onLoadNextLevel != null)
+        {
+            onLoadNextLevel();
+        }
+    }
+
+
+}
